7b094f9b038d3de642b646df79a822696f84032a
lgh <lcliguohao@163.com>
2015-05-29 16:51:49 +0800

    init_airmap

--- mapview/templates/airmapinit.js
+++ mapview/templates/airmapinit.js
@@ -7,8 +7,17 @@ function init_mapstylelist(sel) {
 	sel.value = 'midnight';
 }
 
-function init_map() {
+function init_airmap() {
+	map.addControl(new BMap.NavigationControl());               // 添加平移缩放控件
+	map.addControl(new BMap.ScaleControl());                    // 添加比例尺控件
+	map.addControl(new BMap.OverviewMapControl());              //添加缩略地图控件
+	map.addControl(new BMap.MapTypeControl());          //添加地图类型控件
 
+    map.enableScrollWheelZoom(); // 允许滚轮缩放
+	map.disable3DBuilding();
+
+    var center_point = new BMap.Point(116.4, 39.92);
+    map.centerAndZoom(center_point, 15);             // 初始化地图，设置中心点坐标和地图级别
 }
 
 function init_timeline() {
